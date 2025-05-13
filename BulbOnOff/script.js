4be645a74047dcdd6f29aa046b23169b5b3a9fb7
Sheetu1 <sheetusharma548@gmail.com>
2025-05-13 19:14:44 +0530

    Structure done now let's move to the feature part

--- BulbOnOff/script.js
+++ BulbOnOff/script.js
@@ -1,1 +1,7 @@
 console.log("Hello");
+
+let On = document.querySelector("div")
+let btn = document.querySelector("button")
+btn.addEventListener("click", () => {
+    On.style.backgroundColor = "yellow"
+} ) 

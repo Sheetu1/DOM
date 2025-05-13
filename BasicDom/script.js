9cc1d08e61e3e28f3a80676c91606819dbcbbfc6
Sheetu1 <sheetusharma548@gmail.com>
2025-05-13 18:34:01 +0530

    change html,css

--- BasicDom/script.js
+++ BasicDom/script.js
@@ -1,2 +1,23 @@
-console.log("Hello");
-const h1 = document.querySelector("h1");
+
+// selection of an element
+// let a = document.querySelector("h1");
+
+// changing HTML
+// a.innerHTML = "Document Object Model"
+
+// changing CSS
+// a.style.color = "red";
+
+// Event Listener
+// a.addEventListener("click",()=>{
+//     console.log("hey");   
+// })
+
+
+let a = document.querySelector("h1");
+a.innerHTML = "Document Object Model"
+a.style.color = "red";
+a.addEventListener("click",()=>{
+    console.log("hey");   
+})
+

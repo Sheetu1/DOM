77007f9e6335b17ce7940cca4721b21599a591de
Sheetu1 <sheetusharma548@gmail.com>
2025-05-13 19:31:50 +0530

    done

--- BasicDom/script.js
+++ BasicDom/script.js
@@ -16,12 +16,12 @@
 // })
 
 
-// let a = document.querySelector("h1");
-// a.innerHTML = "Document Object Model"
-// a.style.color = "red";
-// a.addEventListener("click",()=>{
-//     // console.log("hey");  
-//     a.innerHTML = "Change ho gya yrr tu to"
-//     a.style.color = "blue"
-// })
+let a = document.querySelector("h1");
+a.innerHTML = "Document Object Model"
+a.style.color = "red";
+a.addEventListener("click",()=>{
+    console.log("hey");  
+    a.innerHTML = "Change ho gya yrr tu to"
+    a.style.color = "blue"
+})
 

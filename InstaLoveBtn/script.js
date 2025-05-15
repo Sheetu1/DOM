f4cfdf8f2badeda5673fb666ccdf0b00f45cffb2
Sheetu1 <sheetusharma548@gmail.com>
2025-05-15 07:48:42 +0530

    Like feature done

--- InstaLoveBtn/script.js
+++ InstaLoveBtn/script.js
@@ -1,4 +1,16 @@
-let love = document.querySelector('i');
-let image = document.querySelector("img");
 let container = document.querySelector("#container");
+let love = document.querySelector("i");
+
+container.addEventListener("dblclick", () => {
+  love.style.transform = "translate(-50% , -50%) scale(2)";
+  love.style.opacity = 0.8;
+  love.style.color = "red";
+  setTimeout(() => {
+  love.style.opacity = 0;
+},1000);
+
+setTimeout(() => {
+  love.style.transform = "translate(-50% , -50%) scale(0)";
+},2000);
+});
 

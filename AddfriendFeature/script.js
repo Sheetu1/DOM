7d8d241ad8adb2fe7aee052b31d79f879cf25fba
Sheetu1 <sheetusharma548@gmail.com>
2025-05-15 06:14:04 +0530

    now let's AddFriends and RemoveFriends in single btns

--- AddfriendFeature/script.js
+++ AddfriendFeature/script.js
@@ -1,12 +1,17 @@
-let remove = document.getElementById("remove");
-let add = document.getElementById("AddFriends");
-let stranger = document.querySelector("h3");
-add.addEventListener("click", () => {
-stranger.innerHTML = "Friends";
-stranger.style.color = "green";
-})
+// addFriends and remove seprate btns 
 
-remove.addEventListener("click", () => {
-    stranger.innerHTML = "Stranger";
-    stranger.style.color = "red";
-})
+// let remove = document.getElementById("remove");
+// let add = document.getElementById("AddFriends");
+// let stranger = document.querySelector("h3");
+// add.addEventListener("click", () => {
+// stranger.innerHTML = "Friends";
+// stranger.style.color = "green";
+// })
+
+// remove.addEventListener("click", () => {
+//     stranger.innerHTML = "Stranger";
+//     stranger.style.color = "red";
+// })
+
+// but now let's AddFriends and RemoveFriends in single btns
+

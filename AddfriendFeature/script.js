65b396fdff8ddbb3654d7e8ded9e4f78eadae660
Sheetu1 <sheetusharma548@gmail.com>
2025-05-15 06:18:57 +0530

    Done

--- AddfriendFeature/script.js
+++ AddfriendFeature/script.js
@@ -1,4 +1,4 @@
-// addFriends and remove seprate btns 
+// addFriends and remove seprate btns
 
 // let remove = document.getElementById("remove");
 // let add = document.getElementById("AddFriends");
@@ -14,4 +14,18 @@
 // })
 
 // but now let's AddFriends and RemoveFriends in single btns
+let add = document.getElementById("AddFriends");
+let stranger = document.querySelector("h3");
+let flag = 0;
 
+add.addEventListener("click", () => {
+  if (flag == 0) {
+    stranger.innerHTML = "Friends";
+    stranger.style.color = "green";
+    flag = 1;
+  } else {
+    stranger.innerHTML = "Stranger";
+    stranger.style.color = "red";
+    flag = 0;
+  }
+});

d6565d2a1c574f930a8d4bb8c647cd79b185a8ad
Sheetu1 <sheetusharma548@gmail.com>
2025-05-17 21:31:02 +0530

    complete feature

--- CustomCursor/script.js
+++ CustomCursor/script.js
@@ -1,1 +1,7 @@
-const main = document.querySelector(main)
+const main = document.querySelector("#main")
+const crsr = document.querySelector(".cursor")
+
+main.addEventListener("mousemove", (dets) => {
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+})
